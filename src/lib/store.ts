3742eb76f45b460196d3f5390c5ed7aa648d73bf
Saturn Maoudis <saturnmaoudis@protonmail.com>
2026-02-19 19:41:29 +0000

    maunual store save

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,7 +4,7 @@ import type { Reminder } from "./types/store.type";
 // we cant just await the store as webkit will break </3
 const getStore = load("store.json", {
 	defaults: {},
-	autoSave: 100,
+	autoSave: false,
 });
 
 export const getReminders = async () => {
@@ -23,15 +23,18 @@ export const newReminder = async () => {
 	const store = await getStore;
 	const id = Date.now().toString();
 	await store.set(id, { id, reminder: { title: "Untitled Reminder" } });
+	await store.save();
 	return id;
 };
 
 export const saveReminder = async (id: string, reminder: Reminder) => {
 	const store = await getStore;
 	await store.set(id, reminder);
+	await store.save();
 };
 
 export const deleteReminder = async (id: string) => {
 	const store = await getStore;
 	await store.delete(id);
+	await store.save();
 };
