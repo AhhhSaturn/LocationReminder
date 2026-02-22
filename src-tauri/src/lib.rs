efc6fe1d1bf36809258c98a3a24a29002786686e
Saturn Maoudis <saturnmaoudis@protonmail.com>
2026-02-22 10:52:33 +0000

    get google maps api key from rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,11 @@
+#[macro_use]
+extern crate dotenv_codegen;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_key() -> String {
+    let secret = dotenv!("MAPS_KEY");
+    format!("{}", secret)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -10,7 +14,7 @@ pub fn run() {
         .plugin(tauri_plugin_geolocation::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_key])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
